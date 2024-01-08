9a39a506ac996e494aaffd0639067cb81fdbd1c7
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2024-01-08 21:55:51 +0000

    Falta relacion

--- src/models.py
+++ src/models.py
@@ -7,23 +7,31 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    user_name = Column(String(50), nullable=False)
+    password = Column(String(200))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Pokemons(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String)
+    type = Column(String)
+    habilities = Column(String)
+    user_id = Column(Integer, ForeignKey('user_id'))
+    user = relationship(User)
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    pokemon_id = Column(String)
+    user_id = Column(String)
+    user_id = Column(Integer, ForeignKey('user_id'))
+    user = relationship(User)
+    
 
     def to_dict(self):
         return {}
